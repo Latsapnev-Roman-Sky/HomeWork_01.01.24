728fd8391ae3eee43175805aa51ceffce2403d1f
latsa <latsapnev01@mail.ru>
2023-12-31 17:14:05 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Task 1
+
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        int[] array3 = new int[3];
+        array3[0] = 99;
+        array3[1] = 999;
+        array3[2] = 9999;
+
     }
 }
