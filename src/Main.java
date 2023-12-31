dd808871d88b2149771abe2fed318c2337e6a53f
latsa <latsapnev01@mail.ru>
2023-12-31 17:14:45 +0300

    Task №2

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,22 @@ public class Main {
         array3[1] = 999;
         array3[2] = 9999;
 
+        //Task 2
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i] + ", ");
+        }
+        System.out.println();
+
     }
 }
